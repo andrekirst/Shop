55bffda62ade01864f423e7cf37d477e50c0d6a1
André Kirst <andre.kirst@comparex.de>
2019-02-08 12:58:36 +0100

    Erste Solution hinzugefügt

---
+++ src/Shop/ProductSearchService/ProductSearchService.API/DataAccess/ProductSearchDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductSearchService.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductSearchService.API.DataAccess
+{
+    public class ProductSearchDbContext : DbContext
+    {
+        public ProductSearchDbContext(DbContextOptions<ProductSearchDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(m => m.Productnumber);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+    }
+}
---
+++ src/Shop/ProductSearchService/ProductSearchService.API/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductSearchService.API.Model
+{
+    [Table(name: "Products")]
+    public class Product
+    {
+        public string Productnumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}

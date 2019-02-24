a3dcadd70493be7a858c112852a5f0e05d8da458
André Kirst <github@andrekirst.de>
2019-02-24 23:30:51 +0100

    Added search byx filter

--- src/Shop/ProductSearchService/ProductSearchService.API/Controllers/ProductController.cs
+++ src/Shop/ProductSearchService/ProductSearchService.API/Controllers/ProductController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductSearchService.API.DataAccess;
 using ProductSearchService.API.Model;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace ProductSearchService.API.Controllers
 {
@@ -11,25 +16,60 @@ namespace ProductSearchService.API.Controllers
     public class ProductController : ControllerBase
     {
         private readonly ProductSearchDbContext _dbContext;
+        private readonly ILogger<ProductController> _logger;
 
-        public ProductController(ProductSearchDbContext dbContext)
+        public ProductController(
+            ProductSearchDbContext dbContext,
+            ILogger<ProductController> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         [HttpGet]
         [ProducesResponseType(statusCode: 404)]
         [ProducesResponseType(statusCode: 200, Type = typeof(Product))]
         [Route("{productnumber}", Name = "GetByProductnumber")]
-        public async Task<ActionResult<Product>> GetByProductnumber(string productnumber)
+        public async Task<ActionResult<Product>> GetByProductnumber(string productnumber, CancellationToken cancellationToken)
         {
-            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Productnumber == productnumber);
-            if (product == null)
+            try
             {
-                return NotFound();
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Productnumber == productnumber, cancellationToken: cancellationToken);
+
+                return Ok(product);
+            }
+            catch (TaskCanceledException exception)
+            {
+                _logger.LogError(exception, message: $"GetByProductnumber \"{productnumber}\" cancelled");
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(statusCode: 404)]
+        [ProducesResponseType(statusCode: 200, Type = typeof(List<Product>))]
+        [Route("byfilter/{filter}", Name = "GetByFilter")]
+        public async Task<ActionResult<List<Product>>> GetByFilter(string filter, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var filterParameter = new SqlParameter("filter", filter);
+                var products = await _dbContext.Products.FromSql(
+                        "SELECT [ProductId], [Productnumber], [Name], [Description] FROM [dbo].[Products]" +
+                        "WHERE [Productnumber] LIKE '%' + @Filter + '%'" +
+                        "OR [Name] LIKE '%' + @Filter + '%'" +
+                        "OR [Description] LIKE '%' + @Filter + '%'",
+                        filterParameter)
+                        .ToListAsync(cancellationToken: cancellationToken);
+
+                return Ok(products);
+            }
+            catch (TaskCanceledException exception)
+            {
+                _logger.LogError(exception, message: $"GetByFilter \"{filter}\" cancelled");
             }
 
-            return Ok(product);
+            return NotFound();
         }
     }
 }

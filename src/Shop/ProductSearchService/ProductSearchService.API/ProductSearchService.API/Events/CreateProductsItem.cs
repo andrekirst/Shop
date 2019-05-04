a86bf9387a4e59e7dd3d274947be8cb21cacc701
André Kirst <github@andrekirst.de>
2019-05-04 22:40:18 +0200

    Add new Event ProductsCreateEvent (WIP)

---
+++ src/Shop/ProductSearchService/ProductSearchService.API/ProductSearchService.API/Events/CreateProductsItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductSearchService.API.Events
+{
+    public class CreateProductsItem
+    {
+        public string Productnumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}

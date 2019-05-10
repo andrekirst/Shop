27e767aa5632f04835b2b831bdbafe850745adef
André Kirst <github@andrekirst.de>
2019-05-10 14:39:43 +0200

    JSON Serializer

---
+++ src/Shop/ProductSearchService/ProductSearchService.API/ProductSearchService.API/Infrastructure/Json/NetCoreJsonSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProductSearchService.API.Infrastructure.Json
+{
+    public class NetCoreJsonSerializer : IJsonSerializer
+    {
+        public string Serialize(object value)
+            => JsonSerializer.ToString(value: value);
+
+        public T Deserialize<T>(string json)
+            => JsonSerializer.Parse<T>(json: json);
+
+        public dynamic Parse(string json)
+            => JsonSerializer.Parse<dynamic>(json: json);
+    }
+}
